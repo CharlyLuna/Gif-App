daf75c76e4a63b33b38466391baf7453b8a271cf
CharlyLuna <cluna10@ucol.mx>
2023-01-04 12:54:08 -0600

    Change way of imports in GifApp

--- src/GifApp.jsx
+++ src/GifApp.jsx
@@ -1,7 +1,5 @@
 import { useState } from "react";
-import { AddCategory } from "./components/AddCategory";
-import { GifGrid } from "./components/GifGrid";
-import { ResetButton } from "./components/ResetButton";
+import { AddCategory, GifGrid, ResetButton } from "./components";
 
 export const GifApp = () => {
   const [categories, setCategories] = useState([]);
@@ -12,17 +10,13 @@ export const GifApp = () => {
   };
 
   const onResetCategories = () => {
-    console.log("reset");
     setCategories([]);
   };
 
   return (
     <>
       <h1>GifApp</h1>
-      <AddCategory
-        // setCategories={onAddCategory}
-        onNewCategory={(value) => onAddCategory(value)}
-      />
+      <AddCategory onNewCategory={(value) => onAddCategory(value)} />
       <ResetButton onReset={onResetCategories} />
       {categories.map((category) => (
         <GifGrid key={category} category={category} />
